4591936bb415d0ec69a36c461c50222fe6ebfb91
jaobrabo123 <jp2904.lindo@gmail.com>
2026-09-16 15:07:20 -0300

    docs: adiciona JSDocs ao PrismaClientModels e exporta ele no index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export type { Prisma7OrmTypes } from "./types/prisma7-orm-types.type";
 export type { Prisma7ClientLike } from "./types/prisma7-client-like.type";
 export type { AdapterRelations } from "./types/adapter-relations.type";
 export type { VSRepoPrisma7AdapterConfig } from "./types/adapter-config.type";
+export type { PrismaClientModels } from "./types/prisma-client-models.type";
